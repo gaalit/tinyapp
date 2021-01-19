24ce8b8a1515158be8498de6f2ea649c1f7de6e0
Galit <galit.gerasimov3@gmail.com>
2021-01-19 00:03:28 +0000

    Added a function that generates a random string

--- express_server.js
+++ express_server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -39,4 +44,10 @@ app.get("/urls", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  console.log(generateRandomString());
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
