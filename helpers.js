d0415f2f31ed820cdb02c3798da31da3caee9de7
Galit <galit.gerasimov3@gmail.com>
2021-01-20 23:26:04 +0000

    Password match function added

--- helpers.js
+++ helpers.js
@@ -2,13 +2,20 @@
 let emailFinder = function(email, userDatabase) {
   for(let user in userDatabase) {
     if(email === userDatabase[user].email) {
-      return true
-    } else {
+      return userDatabase[user].id;
+    }
+    }
+    return false;
+  }
+
+  let passwordMatching = function(password, userDatabase) {
+    for(let user in userDatabase) {
+      if(password === userDatabase[user].password) {
+        return true
+      }
+      }
       return false;
     }
-  }
-}
+  
 
-
-
-module.exports = {emailFinder};
+module.exports = {emailFinder, passwordMatching};
