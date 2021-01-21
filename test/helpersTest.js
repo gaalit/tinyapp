481c96693ce89b60fc9cdb40678662d75356381e
Galit <galit.gerasimov3@gmail.com>
2021-01-21 23:20:19 +0000

    Fixed linting issues and cleaned up the code

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,27 +4,27 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
     assert.equal(user, expectedOutput);
   });
   it('should return undefined for a user that does not have a valid email', function() {
-    const user = getUserByEmail('user3000@example.com', testUsers)
+    const user = getUserByEmail('user3000@example.com', testUsers);
     const expectedOutput = undefined;
     assert.equal(user, expectedOutput);
-  })
+  });
 
 });
