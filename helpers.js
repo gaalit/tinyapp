c8e038ded457922fd63ff596ca982a7646e3d182
Galit <galit.gerasimov3@gmail.com>
2021-01-21 23:19:56 +0000

    Fixed linting issues and cleaned up the code

--- helpers.js
+++ helpers.js
@@ -1,37 +1,36 @@
 const bcrypt = require("bcrypt");
 
-//emailFinder
 let getUserByEmail = function(email, userDatabase) {
-  for(let user in userDatabase) {
-    if(email === userDatabase[user].email) {
+  for (let user in userDatabase) {
+    if (email === userDatabase[user].email) {
       return userDatabase[user].id;
     }
-    }
   }
+};
 
-  let passwordMatching = function(password, user) {  
-      if(bcrypt.compareSync(password, user.password)) {
-        return true
-      }
-      return false;
-    }
+let passwordMatching = function(password, user) {
+  if (bcrypt.compareSync(password, user.password)) {
+    return true;
+  }
+  return false;
+};
   
 //returns a seperate object with all of the short url's associated to a specific user
-    const urlsForUser = function(id, urlDatabase) {
-      const urltoDisplay = {};
-       for(let shortURL in urlDatabase) {      
-        if(urlDatabase[shortURL].userId === id) {
-          urltoDisplay[shortURL] = urlDatabase[shortURL];
-        }
-      }
-      return urltoDisplay;
+const urlsForUser = function(id, urlDatabase) {
+  const urltoDisplay = {};
+  for (let shortURL in urlDatabase) {
+    if (urlDatabase[shortURL].userId === id) {
+      urltoDisplay[shortURL] = urlDatabase[shortURL];
     }
+  }
+  return urltoDisplay;
+};
 
-    const urlBelongToUser = function(id, shortURL, urlDatabase) {
-      if(urlDatabase[shortURL].userId === id) {
-        return true;
-     }
-     return false;
-    }
+const urlBelongToUser = function(id, shortURL, urlDatabase) {
+  if (urlDatabase[shortURL].userId === id) {
+    return true;
+  }
+  return false;
+};
 
 module.exports = { getUserByEmail, passwordMatching, urlsForUser, urlBelongToUser };
